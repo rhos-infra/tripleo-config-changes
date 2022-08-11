ea4b3fb17b37d51f6721811359a2afe5158def15
Bogdan Dobrelya <bdobreli@redhat.com>
2022-08-11 16:31:05 +0200

    OSP17: Multi-cell post tasks to embrace nova-less UC
    This repo is branchless, and needs to cover both deployment methods:
    - With Nova on UC on Train,
    - Without Nova on UC, as of Wallaby
    
    Split the script to match osp16 and osp17 use cases.
    
    In OSP17 cell1.internalapi /etc/hosts record is missing on cell1
    controller, so switch fetching it from central controller, where it is
    present.
    
    Playbook create-nova-cell-v2.yaml adds cell1.internalapi record
    into the central controller /etc/hosts, so drop the no longer required
    ssh command and env vars.
    
    Also adjust the osp17 version of the  script to fail, if any command
    fails.
    For example, ansible fails if a cell already exists.
    
    Related: rhbz#2089512
    Signed-off-by: Bogdan Dobrelya <bdobreli@redhat.com>
    Change-Id: I1cdf9d7ed2ab5e4fce39d0adb2db9f99e083ed8c

--- tasks/multi_cell_v2_osp17/templates/post_tasks.sh
+++ tasks/multi_cell_v2_osp17/templates/post_tasks.sh
@@ -1,17 +1,6 @@
 #!/bin/bash
-source ~/stackrc
-mkdir -p /home/stack/inventories
-
-for i in overcloud cell1; do \
-    /usr/bin/tripleo-ansible-inventory \
-        --static-yaml-inventory /home/stack/inventories/${i}.yaml \
-        --stack ${i}; \
-done
-
-CELL_CTRL_IP=$(openstack server list -f value -c Networks --name cellcontrol-0 | sed 's/ctlplane=//')
-CTRL_IP=$(openstack server list -f value -c Networks --name controller-0 | sed 's/ctlplane=//')
-CELL_INTERNALAPI_INFO=$(ssh heat-admin@${CELL_CTRL_IP} egrep cell1.internalapi /etc/hosts)
-ansible -i /usr/bin/tripleo-ansible-inventory Controller -b -m lineinfile -a "dest=/etc/hosts line=\"$CELL_INTERNALAPI_INFO\""
+set -e
+test -f ~/stackrc && source ~/stackrc
 
 ANSIBLE_HOST_KEY_CHECKING=False \
 ANSIBLE_SSH_RETRIES=3 \
@@ -81,10 +70,9 @@ do
 done
 
 source ~/stackrc
-CTRL=controller-0
-CTRL_IP=$(openstack server list -f value -c Networks --name $CTRL | sed 's/ctlplane=//')
 export CONTAINERCLI='podman'
 set +e
+CTRL_IP=$(ansible-inventory -i ~/overcloud-deploy/overcloud/config-download/overcloud/tripleo-ansible-inventory.yaml --host controller-0 | jq -r .ctlplane_ip)
 $(ssh heat-admin@${CTRL_IP} sudo ${CONTAINERCLI} exec -i -u root nova_conductor \
 nova-manage db archive_deleted_rows --until-complete --all-cells >> /dev/null 2>&1)
 set -e
